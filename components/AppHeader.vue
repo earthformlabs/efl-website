45d11fb8cdfbd29999c92ebf3267e0d9c122e4d4
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-06-20 17:31:49 +0700

    update alt text

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,10 +2,7 @@
   <div class="w-full absolute z-50">
     <div class="container m-auto flex justify-between px-8 py-8">
       <nuxt-link to="/">
-        <img
-          src="~/assets/logos/earthform-logo-white.svg"
-          alt="EarthForm logo"
-        />
+        <img src="~/assets/logos/earthform-logo-white.svg" alt="EarthForm" />
       </nuxt-link>
       <div class="flex gap-8 text-white">
         <NavLinks />
