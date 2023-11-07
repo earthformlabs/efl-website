972c22d3b385d8855f7c5f4531a8061e2980260b
Nathakit Sae-Tan <nat@pupil-labs.com>
2023-11-07 17:04:34 +0700

    add newsletter signup confirmation page

---
+++ pages/newsletter-signup-success.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="pale-yellow-bg">
+    <div class="h-[650px]">
+      <div
+        class="container mx-auto pt-[120px] px-8 py-4 flex items-center justify-center relative z-10 h-full"
+      >
+        <div class="grid gap-6 text-center justify-center">
+          <h1 class="text-4xl">Thank you for subscribing!</h1>
+          <p>Your email has been successfully added to our newsletter.</p>
+          <nuxt-link
+            class="py-3 px-6 rounded-xl bg-ef-cobalt-blue text-white max-w-fit m-auto"
+            to="/"
+            >Back to home
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
